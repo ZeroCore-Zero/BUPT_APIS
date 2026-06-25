a77004ac66155ae2a27c9aa830695ecc15980f42
ZeroCore <esetting@hotmail.com>
2026-06-26 05:03:25 +0800

    feat: introduce BUPTAPI entry point

---
+++ src/buptapi/__init__.py
@@ -0,0 +1,9 @@
+from buptapi.cas import CAS
+from buptapi.ucloud import UCLOUD
+from buptapi.electronic import ELECTRONIC
+
+
+class BUPTAPI:
+    CAS = CAS
+    UCLOUD = UCLOUD
+    ELECTRONIC = ELECTRONIC
