f7fe14644f5fc275877a09f3baa28ecb5e623f88
ZeroCore <esetting@hotmail.com>
2026-06-26 05:12:26 +0800

    rename project

---
+++ src/buptapis/__init__.py
@@ -0,0 +1,9 @@
+from buptapis.cas import CAS
+from buptapis.ucloud import UCLOUD
+from buptapis.electronic import ELECTRONIC
+
+
+class BUPTAPI:
+    CAS = CAS
+    UCLOUD = UCLOUD
+    ELECTRONIC = ELECTRONIC
