3c3303f584a15e4c01d5ff3b910dc5ab2ffecaf1
ZeroCore <esetting@hotmail.com>
2026-06-28 19:24:22 +0800

    feat: update academic and electric enums with additional endpoints

--- src/buptapis/academic.py
+++ src/buptapis/academic.py
@@ -5,3 +5,4 @@ class ACADEMIC(StrEnum):
     LOGIN = "https://jwgl.bupt.edu.cn/jsxsd/xk/LoginToXk"
     ENROLL = "https://jwgl.bupt.edu.cn/jsxsd/xsxk/xklc_list"
     GRADE = "https://jwgl.bupt.edu.cn/jsxsd/kscj/cjcx_list"
+    STATUS = "https://jwgl.bupt.edu.cn/jsxsd/grxx/xsxx"
--- src/buptapis/electric.py
+++ src/buptapis/electric.py
@@ -3,4 +3,6 @@ from enum import StrEnum
 
 class ELECTRIC(StrEnum):
     LOGIN = "https://app.bupt.edu.cn/buptdf/wap/default/chong"
+    HOMEPAGE = "https://app.bupt.edu.cn/buptdf/wap/default/chong"
     SEARCH = "https://app.bupt.edu.cn/buptdf/wap/default/search"
+    PART = "https://app.bupt.edu.cn/buptdf/wap/default/part"
